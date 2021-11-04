236087307f82843a459a0060f78ad60100063fce
JuanCamiloEscobar <juancamiloe18@gmail.com>
2021-11-04 09:25:29 -0500

    se termina ConsultasConductor

--- src/parkingjcda/modelos/ConsultasConductor.java
+++ src/parkingjcda/modelos/ConsultasConductor.java
@@ -4,10 +4,84 @@
  */
 package parkingjcda.modelos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 /**
  *
  * @author JESCOBAR
  */
-public class ConsultasConductor {
+public class ConsultasConductor extends ModeloBD {
+    
+     private PreparedStatement consultaSQL;
+     private ResultSet resultadoSQL;
+     
+     Connection conexion = conectar();
+     Conductor conductor = new Conductor();
+
+    public ConsultasConductor() {
+    }
+     
+        public boolean insertarConductor(Conductor conductor) {
+              
+        
+        String query = "INSERT INTO conductor(id_cedula,nombres,telefono,celular) VALUES (?,?,?,?)";
+        
+        try{
+            consultaSQL=conexion.prepareStatement(query);
+            
+            consultaSQL.setInt(1, conductor.getIdCedula());
+            consultaSQL.setString(2, conductor.getNombres());
+            consultaSQL.setInt(3, conductor.getTelefono());
+            consultaSQL.setInt(4, conductor.getCelular());
+            
+            int resultado = consultaSQL.executeUpdate();
+            
+            if(resultado>0){
+                return true;
+            }else{
+                return false;
+            }
+                
+        }catch(Exception error){
+            System.out.println("upssss.. "+error);
+            return false;
+        }
+        
+    }
+        
+        public Conductor buscarConductor(int idCedula){        
+       
+        String query="SELECT * FROM conductor WHERE id_cedula=?";        
+        
+        try{            
+            consultaSQL=conexion.prepareStatement(query);
+            
+            consultaSQL.setInt(1,idCedula);
+            
+            resultadoSQL=consultaSQL.executeQuery();
+            
+            if(resultadoSQL.next()){
+                
+                conductor.setIdCedula(resultadoSQL.getInt("id_cedula"));
+                conductor.setNombres(resultadoSQL.getString("nombres"));
+                conductor.setTelefono(resultadoSQL.getInt("telefono"));
+                conductor.setCelular(resultadoSQL.getInt("celular"));
+                return conductor;
+                
+            }else{             
+                return null;                
+            }                
+           
+        }catch(Exception error){
+            
+            System.out.println("upssss.. "+error);
+            return null;
+        }       
+          
+   }
+        
+        
     
 }
